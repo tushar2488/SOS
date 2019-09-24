bab9e47accdcb4240d6a1c2ea5d7d15e9db5ca72
tushar <tushar.tajne@youtility.in>
2019-09-24 15:14:22 +0530

    Salary Payout Changes

--- sos/sos/doctype/salary_payout/salary_payout.js
+++ sos/sos/doctype/salary_payout/salary_payout.js
@@ -2,29 +2,45 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Salary Payout', {
-	refresh: function(doc, dt, dn) {
-		var me = this;
-		if(cur_frm.doc.docstatus==1) {
-			doc.add_custom_button(__('Make Bank Entry'),function() { me.make_bank_payment(doc, dt, dn) });
+	refresh: function(frm) {
+		if(frm.doc.docstatus==0) {
+            //frm.page.clear_primary_action();
+            frm.add_custom_button(__("Get Salary Details"),
+                function() {
+                    frm.events.get_salary_slip_data(frm);
+                }
+            ).toggleClass('btn-primary', !(frm.doc.empdata || []).length);
 		}
 	},
+	clear_employee_payout_table: function (frm) {
+		frm.clear_table('empdata');
+		frm.refresh();
+	},
 	start_date: function(frm) {
-		frm.set_value("bank_name",'');
+		frm.events.clear_employee_table(frm);
 	},
 	end_date: function(frm) {
-		frm.set_value("bank_name",'');
+		frm.events.clear_employee_table(frm);
 	},
 	bank_name: function(frm) {
-		//frappe.msgprint("Selected Bank is : "+frm.doc.bank_name);
-		frm.set_value("payment_account",'');
-		frm.events.get_salary_slip_data(frm);
+		frm.events.clear_employee_table(frm);
 	},
-
 	get_salary_slip_data: function(frm) {
+	    return frappe.call({
+			doc: frm.doc,
+			method: 'get_employees_salary_details',
+			callback: function(r) {
+				if (r.docs[0].empdata){
+					frm.save();
+					frm.refresh();
+                }
+            }
+        });
+    }
 		//frm.events.check_mandatory_to_fetch(frm);
-		if(frm.doc.start_date != null && frm.doc.start_date != null && frm.doc.start_date < frm.doc.end_date){
+		/*if(frm.doc.start_date != null && frm.doc.start_date != null && frm.doc.start_date < frm.doc.end_date){
 			frappe.call({
-				method: "sos.sos.doctype.salary_payout.salary_payout.salary_slip_data",
+				method: "salary_slip_data",
 				args: {
 					"bank_name": frm.doc.bank_name,
 					"start_date": frm.doc.start_date,
@@ -37,7 +53,7 @@ frappe.ui.form.on('Salary Payout', {
 						var total_val=0;
 							$.each(r.message, function(i, d) {
 							var row = frappe.model.add_child(frm.doc, "Salary Payout Detail", "empdata");
-		            					row.employee = d.employee;
+		            			row.employee = d.employee;
 								row.employee_name = d.employee_name;
 								row.ifsc_code = d.ifsc_code;
 								row.bank_ac_no = d.bank_ac_no;
@@ -62,25 +78,5 @@ frappe.ui.form.on('Salary Payout', {
 		}else{
 			frappe.msgprint("Select valid Start date and End date");
 		}
-	}	
+	}	*/
 });
-make_bank_payment = function(doc, cdt, cdn) {
-	console.log("@@@@@ make_bank_payment:::::")
-
-	if (cur_frm.doc.company && cur_frm.doc.start_date && cur_frm.doc.end_date) {
-		return cur_frm.call({
-			//doc: doc,
-			method: "make_bank_payment_entry",
-			doc: cur_frm.doc,
-			callback: function (r) {
-				if (r.message)
-					var doc = frappe.model.sync(r.message)[0];
-					frappe.set_route("Form", doc.doctype, doc.name);
-				}
-			});
-			console.log("@@@@@ Empdata:::::",doc.empdata.length)
-
-		} else {
-				frappe.msgprint(__("Company, From Date and To Date is mandatory"));
-	}
-}
--- sos/sos/doctype/salary_payout/salary_payout.py
+++ sos/sos/doctype/salary_payout/salary_payout.py
@@ -3,8 +3,39 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+import frappe, erpnext
+from frappe import _
 from frappe.model.document import Document
+from frappe.utils import add_days, cint, cstr, flt, getdate, rounded, date_diff, money_in_words
 
 class SalaryPayout(Document):
-	pass
+	def validate(self):
+		company_currency = erpnext.get_company_currency(self.company)
+		self.total_in_words = money_in_words(self.total, company_currency)
+
+	def get_employees_salary_details(self):
+		self.set('empdata', [])
+		ss_list = self.get_ss_list()
+		print("::::::::::::: Salary Slip List::::::::::::: %s" % ss_list)
+		if not ss_list:
+			frappe.throw(_("No salary slips created for the mentioned criteria"))
+		self.total=0
+		for d in ss_list:
+			self.total += d.net_pay
+			self.append('empdata', {'employee':d.employee,
+				'employee_name': d.employee_name,
+				'bank_name': d.bank_name,
+				'bank_ac_no': d.bank_account_no,
+				'net_pay': d.net_pay,
+				'rounded_total': d.rounded_total})
+
+	def get_ss_list(self):
+		""" Returns list of salary slips based on selected criteria """
+		ss_list = frappe.db.sql("""
+			SELECT * FROM `tabSalary Slip` t1
+			WHERE t1.docstatus = 1
+			and t1.start_date >= '%s'
+			and t1.end_date <= '%s'
+			and t1.bank_name = '%s'
+			ORDER BY t1.bank_name DESC """% (self.start_date, self.end_date, self.bank_name), as_dict=True)
+		return ss_list
